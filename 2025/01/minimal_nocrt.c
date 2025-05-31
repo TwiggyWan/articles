e25f35114e86c15f98be3fc004616ed3f17a3a6c
twiggy <none@none.com>
2025-05-31 03:04:16 +0200

    fix minimal_nocrt segfault

--- 2025/01/minimal_nocrt.c
+++ 2025/01/minimal_nocrt.c
@@ -1,2 +1,7 @@
-void_start(void){
+void _start(void){
+    __asm__ volatile (
+		"xorl %edi, %edi\n"  // status = 0
+        "movl $60, %eax\n"   // syscall number for exit
+        "syscall\n"
+    );
 }
